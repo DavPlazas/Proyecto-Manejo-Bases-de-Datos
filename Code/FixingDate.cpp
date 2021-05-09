997ec7b2112b91807b4049953f380ece5cc9fff3
DavPlazas <72804930+DavPlazas@users.noreply.github.com>
2021-05-08 19:50:51 -0500

    Mejora del código anterior

--- Code/FixingDate.cpp
+++ Code/FixingDate.cpp
@@ -4,34 +4,27 @@ using namespace std;
 
 
 int main(){
-  ifstream ifs("ultimaprueba2.txt"); //Nombre del archivo de lectura
+  ifstream ifs("fechasnacimiento.txt"); //Nombre del archivo de lectura
   ofstream ofs("definitivo.txt",ios::trunc); //Nombre del archivo de escritura
-  string linea=""; //String para leer la fecha de nacimiento (INCORRECTA)
-  if(!ifs.eof()){ //Si no es el final del archivo, que empiece a leer el archivo
+  string linea; //String para leer la fecha de nacimiento (INCORRECTA)
+  if(!ifs.eof()){ //Si no es el final del archivo, que empieze a leer el arciho
     while(!ifs.eof()){ // Mientras no sea el final, continue
-      getline(ifs,linea);
-      //cout<<linea<<endl;
+      //getline(ifs,linea);
+      ifs>>linea;
+      if(linea.size()==9)
+        linea='0'+linea;
+      cout<<linea<<endl;
       string temp_dia="";
       string temp_mes="";
       string temp_anio="";
       string total="";
       for (int j=0;j<linea.size();j++){
-        if(linea.size()==11 || linea.size()==10){ //Tamaño del string linea
-          if(j<2)
-            temp_mes+=linea[j];
-          else if(j>2 && j<5)
-            temp_dia+=linea[j];
-          else if(j>5 && j<linea.size())
-            temp_anio+=linea[j];
-        }
-        else if(linea.size()==9){ //tamaño del string linea
-          if(j<1)
-            temp_dia+=linea[j];
-          else if(j<4 && j>1)
-            temp_mes+=linea[j];
-          else if(j>4 && j<linea.size())
-            temp_anio+=linea[j];
-        }
+        if(j<2)
+          temp_mes+=linea[j];
+        else if(j>2 && j<5)
+          temp_dia+=linea[j];
+        else if(j>5 && j<linea.size())
+          temp_anio+=linea[j];
       }
       //total=temp_dia+"/"+temp_mes+"/"+temp_anio; //otro tipo de formato para la fecha
       total=temp_anio+"-"+temp_mes+"-"+temp_dia;
