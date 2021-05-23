9ad59b15c41fd0319955db1e6c7a2f4c984d9983
DavPlazas <72804930+DavPlazas@users.noreply.github.com>
2021-05-22 22:23:16 -0500

    Update ProyectSQL.py

--- Code/ProyectSQL.py
+++ Code/ProyectSQL.py
@@ -5,6 +5,7 @@ Created on Sat May 22 21:35:49 2021
 @author: David
 """
 
+#Funcion para determinar el promedio obtenido en el area de ingles por departamentos
 def avgIngles():
     return ''' select avg(x.puntaje_ingles)::real as promingles, d.nombre_d as departamento from 
     ((((examen x inner join estudiante e on x.id_estudiante=e.id_estudiante)
@@ -12,6 +13,7 @@ def avgIngles():
     inner join departamento d on d.codigo_d=m.codigo_d_departamento) 
     where x.puntaje_ingles is not null group by d.nombre_d;'''
 
+#Promedio de cada asignatura para los hombres(H)
 def avgSubjectsM():
     return ''' select e.sexo,avg(x.puntaje_c_naturales)::real as promNaturales,
     avg(x.puntaje_matematicas)::real as promMatematicas,
@@ -20,7 +22,8 @@ def avgSubjectsM():
     avg(x.puntaje_lectura_critica)::real as promLectura 
     from examen x inner join estudiante e on e.id_estudiante=x.id_estudiante 
     group by e.sexo having e.sexo<>'-' and e.sexo='M';'''
-    
+
+#Promedio de cada asignatura para las mujeres(F)
 def avgSubjectsF():
     return ''' select e.sexo,avg(x.puntaje_c_naturales)::real as promNaturales,
     avg(x.puntaje_matematicas)::real as promMatematicas,
